5799a690dcfd9469a7cc1b677cde4bb0ffedeb7b
Maryam Fadaee <maryamfadaee@gmail.com>
2024-12-04 00:22:24 -0500

    add validations to apis

--- books.py
+++ books.py
@@ -1,98 +1,113 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel, Field
+from typing import Optional
+from fastapi.exceptions import HTTPException
+from starlette import status
 
 app = FastAPI()
 
+class Book:
+  id: int
+  title: str
+  author: str
+  description : str
+  rating: int
+  publishedDate: int
 
-BOOKS=[
-  {
-    'title': "To Kill a Mockingbird",
-    'author': "Harper Lee",
-    'category': "Fiction"
-  },
-  {
-    'title': "1984",
-    'author': "George Orwell",
-    'category': "Dystopian"
-  },
-  {
-    'title': "A Brief History of Time",
-    'author': "Stephen Hawking",
-    'category': "Science"
-  },
-  {
-    'title': "The Great Gatsby",
-    'author': "F. Scott Fitzgerald",
-    'category': "Classic"
-  },
-  {
-    'title': "Becoming",
-    'author': "Michelle Obama",
-    'category': "Biography"
-  },
-  {
-    'title': "The Catcher in the Rye",
-    'author': "J.D. Salinger",
-    'category': "Fiction"
+  def __init__(self, id: int, title: str, author: str, description: str, rating: int, publishedDate: int):
+    self.id = id
+    self.title = title
+    self.author = author
+    self.description = description
+    self.rating = rating
+    self.publishedDate = publishedDate
+
+class BookRequest(BaseModel):
+  id: Optional[int] = Field(description='ID is not needed on create request', default=None)
+  title: str =Field(min_length=3)
+  author: str = Field(min_length=1)
+  description: str = Field(min_length=1, max_length=100)
+  rating: int = Field(gt=0, lt=6)
+  publishedDate: int = Field(gt=1900)
+
+  model_config = {
+    "json_schema_extra":{
+      "example": {
+        "title": "Sample Book",
+        "author": "Sample Author",
+        "description": "Sample Description",
+        "rating": 5,
+        "publishedDate":2012
+      }
+    }
   }
-];
+  
+Books = [
+    Book(1, "Book 1", "Author 1", "Description 1", 5, 2012),
+    Book(2, "Book 2", "Author 2", "Description 2", 4, 2013),
+    Book(3, "Book 3", "Author 3", "Description 3", 3, 2014),
+    Book(4, "Book 4", "Author 4", "Description 4", 2, 2015),
+    Book(5, "Book 5", "Author 5", "Description 5", 1, 2012)
+]
+
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def getAllbooks():
+  return Books
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def getBookByPublishDate( publishedDate: int = Query(gt=1999, lt=2031)):
+  booksToReturn = []
+  for book in Books:
+    if book.publishedDate == publishedDate:
+      booksToReturn.append(book)
+  return booksToReturn
+
+@app.get("/books/{bookId}", status_code=status.HTTP_200_OK)
+async def getBookById(bookId: int = Path(gt=0)):
+  for book in Books:
+    if book.id == bookId:
+      return book
+  raise HTTPException(status_code=404, detail='Item not found')
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def getBookByRating( rating: int = Query(gt=0,lt=6)):
+  booksToReturn = []
+  for book in Books:
+    if book.rating == rating:
+      booksToReturn.append(book)
+  return booksToReturn
 
 
-@app.get("/books")
-async def getAllBooks():
-    return BOOKS
+
+@app.post("/createBook", status_code=status.HTTP_201_CREATED)
+async def createBook(bookRequest : BookRequest):
+  newBook = Book(**bookRequest.model_dump())
+  Books.append(addIdToBook(newBook))
+  return bookRequest
 
 
-@app.get("/books/byAuthor/")
-async def getBooksByAuthor(author: str):
-    booksRoReturn = []
-    for book in BOOKS:
-        if book.get('author').casefold() == author.casefold():
-            booksRoReturn.append(book)
-    return booksRoReturn
+def addIdToBook(book: Book):
+    book.id = 1 if len(Books) == 0 else Books[-1].id + 1
+    return book
 
 
-@app.get("/books/{book_title}")
-async def getABookByTitle(book_title:str):
-    for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
-            return book
-        
-
-@app.get("/books/{book_title}/")
-async def getBookByCategoryAndAutor(author: str, category:str):
-    booksRoReturn = []
-    for book in BOOKS:
-        if book.get('author').casefold() == author.casefold() and book.get('category').casefold() == category.casefold():
-            booksRoReturn.append(book)
-    return booksRoReturn
-
-
-@app.get("/books/")
-async def getBooksByCategory(category: str):
-    booksRoReturn = []
-    for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
-            booksRoReturn.append(book)
-    return booksRoReturn
-
-
-@app.post("/books/addBook")
-async def addBook(newBook=Body()):
-    BOOKS.append(newBook)
-    return newBook
-
-
-@app.put("/books/updateBook")
-async def updateBook(updatedBook=Body()):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updatedBook.get('title').casefold():
-            BOOKS[i] = updatedBook
-            return updatedBook
-        
-
-@app.delete("/books/deleteBook/{book_title}")
-async def deleteBook(book_title:str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
-            return {"message": f"Book {book_title} deleted successfully"}
+@app.put("/books/updateBook", status_code=status.HTTP_204_NO_CONTENT)
+async def updateBook(book: BookRequest):
+    bookChanged = False
+    for i in range(len(Books)) :
+        if Books[i].id == book.id:
+            Books[i] = book
+            bookChanged = True
+    if not bookChanged:
+        raise HTTPException(status_code=404, detail='Item not found')
+  
+  
+@app.delete("/books/{bookId}")
+async def deleteBook(bookId: int = Path(gt=0)):
+    bookChanged = False
+    for i in range(len(Books)):
+        if Books[i].id == bookId:
+            Books.pop(i)
+            bookChanged = True
+    if not bookChanged:
+        raise HTTPException(status_code=404, detail='Item not found')
